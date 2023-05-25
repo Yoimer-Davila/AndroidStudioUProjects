8f44c6e7741ac4e690aa6d5855a630b6b0f002db
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2023-05-25 14:41:09 -0500

    Flutter Route Sample Application

---
+++ WeekTenFlutterRoute/week_ten_flutter_route/android/app/src/main/kotlin/com/yoimer/week_ten_flutter_route/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yoimer.week_ten_flutter_route
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ WeekTenFlutterRoute/week_ten_flutter_route/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("First Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondRoute()));
+          },
+          child: const Text("Open Second Route"),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Open First Route"),
+        ),
+      ),
+    );
+  }
+}
+
+
