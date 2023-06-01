25716d43170724514a99bfb9aca4df848f599d25
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2023-06-01 15:56:03 -0500

    Added favorite screen

--- WeekTenFlutterWordsGenerator/week_ten_flutter_words_generator/lib/main.dart
+++ WeekTenFlutterWordsGenerator/week_ten_flutter_words_generator/lib/main.dart
@@ -8,11 +8,37 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: RandomWords(),
+    );
+  }
+}
+
+class FavoriteWords extends StatelessWidget {
+  final Set<WordPair> words;
+  final TextStyle textStyle;
+
+  const FavoriteWords({Key? key, required this.words, required this.textStyle}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Favorites words"),
+      ),
+      body: ListView(
+        children: ListTile.divideTiles(
+            tiles: words.map((pair) => ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: textStyle,
+              ),
+            )),
+            context: context
+        ).toList(),
+      ),
     );
   }
 }
@@ -28,13 +54,23 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final List<WordPair> suggestions = [];
   final TextStyle biggerFont = const TextStyle(fontSize: 18);
-
+  final saved = <WordPair>{};
 
   Widget buildRow(WordPair pair) {
+    final alreadySaved = saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: biggerFont,
+      ),
+      trailing: IconButton(
+        onPressed: () {
+          setState(() {
+            alreadySaved ? saved.remove(pair) : saved.add(pair);
+          });
+        },
+        icon: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border),
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
@@ -58,13 +94,29 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void toSaved() {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => FavoriteWords(
+          words: saved,
+          textStyle: biggerFont,
+        ))
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Random Words"),
+        actions: <Widget>[
+          IconButton(
+            onPressed: toSaved,
+            icon: const Icon(Icons.list),
+          )
+        ],
       ),
       body: buildSuggestions(),
     );
   }
+
 }
